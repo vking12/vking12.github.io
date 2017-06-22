f424f6dd114553b39087c56b2201dc508412f4f0
vking12 <vijayrajmahida@gmail.com>
2017-06-22 09:10:35 -0500

    Images made using canvas.

---
+++ Vis_Tool2/src/js/checkboxLogic.js
@@ -0,0 +1,7 @@
+function cLogic() {
+    var m14 = document.getElementById("Mar14").checked;
+    var a14 = document.getElementById("Aug14").checked;
+    var n14 = document.getElementById("Nov14").checked;
+
+    document.getElementById("demo").innerHTML = m14;
+}
