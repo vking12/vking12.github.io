10fa3bbf7bf6d16fd3e6339a373f68f17bb5d241
vking12 <vijayrajmahida@gmail.com>
2017-06-26 07:34:48 -0500

    Added version 0.6

---
+++ Vis_Tool2/src/js/main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/* Get or create the application global variable */
+var App = App || {};
+
+/* IIFE to initialize the main entry of the application*/
+(function() {
+
+    // setup the pointer to the scope 'this' variable
+    var self = this;
+
+    /* Entry point of the application */
+    App.start = function()
+    {
+        
+    };
+
+}) ();
